2133d98e4a3a4cc9d462931f60c64d2a4d7827ff
Yenisbel Valle <yenisbelv@gmail.com>
2020-04-12 22:05:53 -0700

    Add gatsby skeleton and plugins

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+// module.exports = {
+//   /* Your site config here */
+//   plugins: []
+// }
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+}
